464681b45b556787e658b2f026808b9caeeb67b0
Ryan Mitchell <ryan@playsidelinesports.com>
2021-12-22 22:59:29 -0800

    Add solution to Day 06-2

--- day06/src/lib.rs
+++ day06/src/lib.rs
@@ -1,45 +1,58 @@
-#[derive(Debug, PartialEq, Clone)]
+use std::{collections::HashMap, hash::Hash};
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
 pub struct LanternFish(i32);
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct School {
-    pub fish: Vec<LanternFish>,
+    pub fish_counts: HashMap<LanternFish, i64>,
     pub days_passed: i32,
 }
 
-impl From<Vec<i32>> for School {
-    fn from(fish_days: Vec<i32>) -> Self {
-        let fish = fish_days
-            .iter()
-            .map(|days| LanternFish(*days))
-            .collect();
+impl From<&Vec<i32>> for School {
+    fn from(fish_days: &Vec<i32>) -> Self {
+        let mut fish_counts = HashMap::new();
 
-        School { fish, days_passed: 0 }
+        for fish in fish_days {
+            *fish_counts.entry(LanternFish(*fish)).or_insert(0) += 1;
+        }
+
+        School {
+            fish_counts,
+            days_passed: 0,
+        }
     }
 }
 
 impl School {
+    pub fn get_total_fish(&self) -> i64 {
+        self.fish_counts.values().sum()
+    }
+
     pub fn tick(&self) -> School {
-        let fish = self.fish
-            .iter()
-            .flat_map(|fish| {
-                let LanternFish(days_remaining) = fish;
+        let mut fish_counts = HashMap::new();
 
-                if days_remaining > &0 {
-                    vec![LanternFish(days_remaining - 1)]
-                } else {
-                    vec![LanternFish(6), LanternFish(8)]
-                }
-            })
-            .collect();
+        for (LanternFish(days_remaining), count) in &self.fish_counts {
+            if days_remaining > &0 {
+                *fish_counts
+                    .entry(LanternFish(days_remaining - 1))
+                    .or_insert(0) += count;
+            } else {
+                *fish_counts.entry(LanternFish(6)).or_insert(0) += count;
+                *fish_counts.entry(LanternFish(8)).or_insert(0) += count;
+            }
+        }
 
-        School { fish, days_passed: self.days_passed + 1 }
+        School {
+            fish_counts,
+            days_passed: self.days_passed + 1,
+        }
     }
 
     pub fn tick_n(&self, days: i32) -> School {
         let mut school = School {
-            fish: self.fish.clone(),
-            days_passed: self.days_passed
+            fish_counts: self.fish_counts.clone(),
+            days_passed: self.days_passed,
         };
 
         for _ in 0..days {
@@ -56,46 +69,19 @@ mod test {
 
     #[test]
     fn it_matches_fixture() {
-        let mut school = School {
-            days_passed: 0,
-            fish: vec![
-                LanternFish(3),
-                LanternFish(4),
-                LanternFish(3),
-                LanternFish(1),
-                LanternFish(2)
-            ]
-        };
+        let mut school = School::from(&vec![3, 4, 3, 1, 2]);
 
         school = school.tick();
 
-        assert_eq!(
-            school.fish,
-            vec![
-                LanternFish(2),
-                LanternFish(3),
-                LanternFish(2),
-                LanternFish(0),
-                LanternFish(1)
-            ]
-        );
+        assert_eq!(school.get_total_fish(), 5);
     }
 
     #[test]
-    fn it_matches_fixture_after_18_days() {
-        let mut school = School {
-            days_passed: 0,
-            fish: vec![
-                LanternFish(3),
-                LanternFish(4),
-                LanternFish(3),
-                LanternFish(1),
-                LanternFish(2)
-            ]
-        };
+    fn it_matches_fixture_after_80_days() {
+        let mut school = School::from(&vec![3, 4, 3, 1, 2]);
 
         school = school.tick_n(80);
 
-        assert_eq!(school.fish.len(), 5934);
+        assert_eq!(school.get_total_fish(), 5934);
     }
 }
--- day06/src/main.rs
+++ day06/src/main.rs
@@ -4,12 +4,12 @@ use day06::School;
 
 fn main() {
     let input: Vec<i32> = read_to_string("data/input.txt")
-        .map(|file| {
-            file.split(",").map(str::parse).flatten().collect()
-        })
+        .map(|file| file.split(",").map(str::parse).flatten().collect())
         .unwrap();
 
-    let school = School::from(input).tick_n(80);
+    let school_1 = School::from(&input).tick_n(80);
+    println!("School size (80 days): {}", school_1.get_total_fish());
 
-    println!("School size: {}", school.fish.len());
+    let school_2 = School::from(&input).tick_n(256);
+    println!("School size (256 days): {}", school_2.get_total_fish());
 }
