a71e69286cad6f45b5e78aca01060836fcb0696c
Ryan Mitchell <ryan@playsidelinesports.com>
2021-12-22 18:04:04 -0800

    Add solution to Day 05-2

--- day05/src/lib.rs
+++ day05/src/lib.rs
@@ -13,35 +13,33 @@ pub struct ThermalVent {
 }
 
 impl ThermalVent {
-    pub fn get_covered_points(&self) -> Option<Vec<Point>> {
-        if self.start.x == self.end.x {
-            let y_min = cmp::min(self.start.y, self.end.y);
-            let y_max = cmp::max(self.start.y, self.end.y);
+    pub fn get_covered_points(&self) -> Vec<Point> {
+        let x_min = cmp::min(self.start.x, self.end.x);
+        let x_max = cmp::max(self.start.x, self.end.x);
+        let y_min = cmp::min(self.start.y, self.end.y);
+        let y_max = cmp::max(self.start.y, self.end.y);
 
-            let points = (y_min..=y_max)
+        if x_min == x_max {
+            return (y_min..=y_max)
                 .map(|y| Point { x: self.start.x, y })
                 .collect();
-
-            return Some(points);
         }
 
-        if self.start.y == self.end.y {
-            let x_min = cmp::min(self.start.x, self.end.x);
-            let x_max = cmp::max(self.start.x, self.end.x);
-
-            let points = (x_min..=x_max)
+        if y_min == y_max {
+            return (x_min..=x_max)
                 .map(|x| Point { x, y: self.start.y })
                 .collect();
-
-            return Some(points);
         }
 
-        println!(
-            "Undefined behavior for diagonal lines, {:?} -> {:?}",
-            self.start, self.end
-        );
+        let x_direction = if self.start.x <= self.end.x { 1 } else { -1 };
+        let y_direction = if self.start.y <= self.end.y { 1 } else { -1 };
 
-        None
+        (0..=(x_max - x_min))
+            .map(|delta| Point {
+                x: self.start.x + delta * x_direction,
+                y: self.start.y + delta * y_direction,
+            })
+            .collect()
     }
 }
 
@@ -49,15 +47,8 @@ pub fn count_overlapping_points(vents: Vec<ThermalVent>) -> HashMap<Point, i32> 
     let mut point_counts = HashMap::new();
 
     for vent in vents {
-        match vent.get_covered_points() {
-            Some(points) => {
-                for point in points {
-                    *point_counts.entry(point).or_insert(0) += 1
-                }
-            }
-            None => {
-                println!("A vent had no covered points")
-            }
+        for point in vent.get_covered_points() {
+            *point_counts.entry(point).or_insert(0) += 1
         }
     }
 
@@ -76,7 +67,7 @@ mod test {
         };
 
         assert_eq!(
-            vent.get_covered_points().unwrap(),
+            vent.get_covered_points(),
             vec![
                 Point { x: 1, y: 1 },
                 Point { x: 1, y: 2 },
@@ -93,12 +84,29 @@ mod test {
         };
 
         assert_eq!(
-            vent.get_covered_points().unwrap(),
+            vent.get_covered_points(),
             vec![
                 Point { x: 7, y: 7 },
                 Point { x: 8, y: 7 },
                 Point { x: 9, y: 7 },
             ]
         )
+    }
+
+    #[test]
+    fn it_correctly_returns_diagonal_points() {
+        let vent = ThermalVent {
+            start: Point { x: 9, y: 7 },
+            end: Point { x: 7, y: 9 },
+        };
+
+        assert_eq!(
+            vent.get_covered_points(),
+            vec![
+                Point { x: 9, y: 7 },
+                Point { x: 8, y: 8 },
+                Point { x: 7, y: 9 },
+            ]
+        );
     }
 }
--- day05/src/read_lines.rs
+++
@@ -1,9 +0,0 @@
-use std::fs;
-
-pub fn from(filepath: &str) -> Vec<String> {
-  fs::read_to_string(filepath)
-    .expect("Unable to read input file")
-    .lines()
-    .map(str::to_string)
-    .collect()
-}
